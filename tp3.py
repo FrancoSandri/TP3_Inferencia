add9435b0996b4e51a998bff3ee87f45031f2d3f
Franco Sandri <francosandriespi@gmail.com>
2025-11-18 18:55:03 -0300

    subi el tp3

---
+++ tp3.py
@@ -0,0 +1,31 @@
+
+# Configuraciones para diferentes posiciones de las anclas (referencias):
+# "pos": indica coordenadas cartesianas (m)
+# "angulo": indica el ángulo del ancla respecto de la vertical (rad)
+
+config_1 = {
+"P1":{"pos": (56.69873, 75.0),"angulo": -1.0471975493043784},
+"P2":{"pos": (94.770655, 109.771682),"angulo": -0.08726646965814552},
+"P3":{"pos": (135.355339, 85.355339),"angulo": 0.7853981633974481}
+}
+
+config_2 = {
+"P1":{"pos": (64.644661, 85.355339),"angulo": -0.7853981633974483},
+"P2":{"pos": (34.467836, 95.886115),"angulo": -0.9599310909314129},
+"P3":{"pos": (39.818498, 129.863551),"angulo": -0.6457718207490258}
+}
+
+config_3 = {
+"P1":{"pos": (64.644661, 85.355339),"angulo": -0.7853981633974483},
+"P2":{"pos": (40.548414, 103.530449),"angulo": -0.8377580360740813},
+"P3":{"pos": (35.721239, 126.604444),"angulo": -0.6981317030426971}
+}
+
+config_4 = {
+"P1":{"pos": (64.644661, 85.355339),"angulo": -0.7853981633974483},
+"P2":{"pos": (43.332813, 106.469726),"angulo": -0.7871434894299918},
+"P3":{"pos": (29.412843, 120.833984),"angulo": -0.7836528320585269}
+}
+
+# Posición esperada para el objetivo
+p = (100, 50)
